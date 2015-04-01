9154d6fe901777341090ae3a95753c9eff33050a
priestleycj <webcommauto@users.noreply.github.com>
2015-04-01 17:50:23 +0000

    Make style updates. Override branding template.

--- drupal/sites/SBIR/themes/sbirtheme/templates/region--branding.tpl.php
+++ drupal/sites/SBIR/themes/sbirtheme/templates/region--branding.tpl.php
@@ -4,19 +4,20 @@
       <div class="branding-data clearfix">
         <?php if ($linked_logo_img): ?>
           <div class="logo-img">
-            <?php print $linked_logo_img; ?>
-          </div>
-        <?php endif; ?>
+            <?php //print $linked_logo_img; ?>
+            <div class="logo-img">
+              <a href="/" rel="home" title="NCI: SBIR & STTR">
+                <img src="/sites/SBIR/themes/sbirtheme/logo.png" alt="SBIR Development Center" id="logo">
         <?php if ($site_name || $site_slogan): ?>
           <?php $class = $site_name_hidden && $site_slogan_hidden ? ' element-invisible' : ''; ?>
           <hgroup class="site-name-slogan<?php print $class; ?>">        
             <?php if ($site_name): ?>
-            <span id="institute-name">National Cancer Institute</span>
+              <span id="institute-name">National Cancer Institute</span>
               <?php $class = $site_name_hidden ? ' element-invisible' : ''; ?>
               <?php if ($is_front): ?>        
-                <h1 class="site-name<?php print $class; ?>"><?php print $linked_site_name; ?></h1>
+                <h1 class="site-name<?php print $class; ?>"><?php print 'SBIR Development Center'; ?></h1>
               <?php else: ?>
-                <h2 class="site-name<?php print $class; ?>"><?php print $linked_site_name; ?></h2>
+                <h2 class="site-name<?php print $class; ?>"><?php print 'SBIR Development Center'; ?></h2>
               <?php endif; ?>
             <?php endif; ?>
             <?php if ($site_slogan): ?>
@@ -24,12 +25,15 @@
               <h6 class="site-slogan<?php print $class; ?>"><?php print $site_slogan; ?></h6>
             <?php endif; ?>
           </hgroup>
-        <?php endif; ?>
-
-      </div>
-            <div id="social-media">
-        <?php print $content; ?>
+        <?php endif; ?>                
+              </a>
+            </div>
           </div>
+        <?php endif; ?>
+      </div>
+      <div id="social-media">
+        <?php print $content; ?>
+      </div>
     <?php endif; ?>
   </div>
 </div>
