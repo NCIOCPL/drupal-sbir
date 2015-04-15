d05a34a6671e4aab47135395c3bc1633529ada86
priestleycj <webcommauto@users.noreply.github.com>
2015-04-15 21:20:49 +0000

    fix null reference bug

--- drupal/sites/SBIR/themes/sbirtheme/templates/page--node.tpl.php
+++ drupal/sites/SBIR/themes/sbirtheme/templates/page--node.tpl.php
@@ -11,28 +11,30 @@
 
   <?php if (isset($page['content'])) : ?>
     <?php print render($page['content']); ?>
-    <?php $date_count = sizeof($node->field_date_display_mode); ?>
-    <?php if ($date_count > 0): ?>
-      <div id="sbir-dates">
-        <?php foreach ($node->field_date_display_mode['und'] as $date_type): ?>
-          <?php if ($date_type['value'] == 'posted' && isset($node->field_posted_date['und'])):
-            ?>
-            <?php $converted_date = format_date($node->field_posted_date['und'][0]['value'], 'sbir_standard'); ?>
-            <div id="sbir-posted-date" class="sbir-date"><span>Posted:</span> <?php print $converted_date; ?></div>
-          <?php endif; ?>
-          <?php if ($date_type['value'] == 'updated' && isset($node->field_updated_date['und'])):
-            ?>
-            <?php $converted_date = format_date($node->field_updated_date['und'][0]['value'], 'sbir_standard'); ?>
-            <div id="sbir-updated-date" class="sbir-date"><span>Updated:</span> <?php print $converted_date; ?></div>
-          <?php endif; ?>   
-          <?php if ($date_type['value'] == 'reviewed' && isset($node->field_reviewed_date['und'])):
-            ?>
-            <?php $converted_date = format_date($node->field_reviewed_date['und'][0]['value'], 'sbir_standard'); ?>
-            <div id="sbir-reviewed-date" class="sbir-date"><span>Reviewed:</span> <?php print $converted_date; ?></div>
-          <?php endif; ?>                   
-        <?php endforeach; ?>
-      </div>
-    <?php endif; ?>  
+    <?php if (isset($node->field_date_display_mode)): ?>
+      <?php $date_count = sizeof($node->field_date_display_mode); ?>
+      <?php if ($date_count > 0): ?>
+        <div id="sbir-dates">
+          <?php foreach ($node->field_date_display_mode['und'] as $date_type): ?>
+            <?php if ($date_type['value'] == 'posted' && isset($node->field_posted_date['und'])):
+              ?>
+              <?php $converted_date = format_date($node->field_posted_date['und'][0]['value'], 'sbir_standard'); ?>
+              <div id="sbir-posted-date" class="sbir-date"><span>Posted:</span> <?php print $converted_date; ?></div>
+            <?php endif; ?>
+            <?php if ($date_type['value'] == 'updated' && isset($node->field_updated_date['und'])):
+              ?>
+              <?php $converted_date = format_date($node->field_updated_date['und'][0]['value'], 'sbir_standard'); ?>
+              <div id="sbir-updated-date" class="sbir-date"><span>Updated:</span> <?php print $converted_date; ?></div>
+            <?php endif; ?>   
+            <?php if ($date_type['value'] == 'reviewed' && isset($node->field_reviewed_date['und'])):
+              ?>
+              <?php $converted_date = format_date($node->field_reviewed_date['und'][0]['value'], 'sbir_standard'); ?>
+              <div id="sbir-reviewed-date" class="sbir-date"><span>Reviewed:</span> <?php print $converted_date; ?></div>
+            <?php endif; ?>                   
+          <?php endforeach; ?>
+        </div>
+      <?php endif; ?>  
+    <?php endif; ?>
   <?php endif; ?>
   <?php if (isset($page['footer'])) : ?>
     <?php print render($page['footer']); ?>
