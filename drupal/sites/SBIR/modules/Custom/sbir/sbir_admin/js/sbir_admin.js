193192958c386e23f699b10abe47626d722aee09
priestleycj <webcommauto@users.noreply.github.com>
2015-05-19 16:17:04 +0000

    turn of autocomplete for user profile form

--- drupal/sites/SBIR/modules/Custom/sbir/sbir_admin/js/sbir_admin.js
+++ drupal/sites/SBIR/modules/Custom/sbir/sbir_admin/js/sbir_admin.js
@@ -1,3 +1,9 @@
 jQuery(document).ready(function () {
+  
+  // on node edit forms, hide the description
+  // field for menu items
   jQuery('.form-item-menu-description').hide();
+  
+    // turn off autocomplete for all password fields
+    jQuery('input[type="password"]').attr('autocomplete', 'off');  
 });
