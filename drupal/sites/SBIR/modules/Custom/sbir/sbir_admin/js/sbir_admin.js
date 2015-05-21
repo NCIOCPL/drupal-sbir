d8688545f72a5cd66d3c119b043976c13187c062
priestleycj <webcommauto@users.noreply.github.com>
2015-05-21 20:29:47 +0000

    hide description field for menu item link when editing a node

--- drupal/sites/SBIR/modules/Custom/sbir/sbir_admin/js/sbir_admin.js
+++ drupal/sites/SBIR/modules/Custom/sbir/sbir_admin/js/sbir_admin.js
@@ -3,7 +3,7 @@
 
     // on node edit forms, hide the description
     // field for menu items
-   // $('.form-item-menu-description').hide();
+    $('.form-item-menu-description').hide();
 
     // turn off autocomplete for all password fields
     $('input[type="password"]').attr('autocomplete', 'off');
