a68e845b1e0848b926ad62ff05ea8e8f3100449d
priestleycj <webcommauto@users.noreply.github.com>
2015-05-26 14:57:46 +0000

    hide certain admin features from non-admins

--- drupal/sites/SBIR/modules/Custom/sbir/sbir_admin/js/sbir_admin.js
+++ drupal/sites/SBIR/modules/Custom/sbir/sbir_admin/js/sbir_admin.js
@@ -3,9 +3,12 @@
 
     // on node edit forms, hide the description
     // field for menu items
-    $('.form-item-menu-description').hide();
+   // $('.form-item-menu-description').hide();
 
     // turn off autocomplete for all password fields
     $('input[type="password"]').attr('autocomplete', 'off');
   });
+  
+  $('a[href="/admin/structure/menu"]').parent().hide();
+  $('.page-admin-structure ul.admin-list li').css('border', 'none');
 }(jQuery));
