8c0a330820e6c2991afa50819c4f2c410c7cd70a
priestleycj <webcommauto@users.noreply.github.com>
2015-06-08 14:52:20 +0000

    fix search and events bugs

--- drupal/sites/sbir/themes/sbirtheme/templates/zone--content.tpl.php
+++ drupal/sites/sbir/themes/sbirtheme/templates/zone--content.tpl.php
@@ -1,11 +1,29 @@
 <?php if ($wrapper): ?><div<?php print $attributes; ?>><?php endif; ?>  
   <div<?php print $content_attributes; ?>>    
     <?php if ($breadcrumb): ?>
-      <!--<div id="breadcrumb" class="grid-<?php print $columns; ?>"><?php print $breadcrumb; ?></div>-->
+              <!--<div id="breadcrumb" class="grid-<?php print $columns; ?>"><?php print $breadcrumb; ?></div>-->
     <?php endif; ?>    
     <?php if ($messages): ?>
       <div id="messages" class="grid-<?php print $columns; ?>"><?php print $messages; ?></div>
     <?php endif; ?>
     <?php print $content; ?>
+    <?php
+    if ((arg(0) == "search") &&
+        arg(1) == "node" &&
+        arg(2) == "") :
+      ?>
+      <div class="block block-system block-main block-system-main odd block-without-title" id="block-system-main">
+        <div class="block-inner clearfix">
+          <div class="content clearfix">
+            <h2>Your search yielded no results</h2>
+            <ul>
+              <li>Check if your spelling is correct.</li>
+              <li>Remove quotes around phrases to search for each word individually. <em>bike shed</em> will often show more results than <em>"bike shed"</em>.</li>
+              <li>Consider loosening your query with <em>OR</em>. <em>bike OR shed</em> will often show more results than <em>bike shed</em>.</li>
+            </ul>
+          </div>
+        </div>
+      </div>      
+    <?php endif; ?>
   </div>
-<?php if ($wrapper): ?></div><?php endif; ?>
+  <?php if ($wrapper): ?></div><?php endif; ?>
