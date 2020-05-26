f56881bb9ec04f2a41a23de198e85153d9f8e911
Erik Meyer <erik.meyer@publicissapient.com>
2020-05-26 12:12:37 -0400

    Updated core to 7.70; also aded new content type Promotional Page with associated paragraph types.  References https://tracker.nci.nih.gov/browse/OCPLSBIR-229.

---
+++ sites/sbir/themes/sbirtheme/templates/node--promotional-page.tpl.php
@@ -0,0 +1,34 @@
+<article<?php print $attributes; ?>>
+    <div<?php print $content_attributes; ?>>
+        <?php
+        // We hide the comments and links now so that we can render them later.
+        hide($content['comments']);
+        hide($content['links']);
+      //  print render($content);
+        ?>
+
+        <?php if (!empty($content['field_header_image'])): ?>
+            <?php print render($content['field_header_image']); ?>
+        <?php endif; ?>
+
+        <?php if (!empty($content['field_display_title'])): ?>
+            <h2><?php print render($content['field_display_title']); ?></h2>
+        <?php endif; ?>
+
+        <?php if (!empty($content['field_button_link_box'])): ?>
+            <?php print render($content['field_button_link_box']); ?>
+        <?php endif; ?>
+
+        <?php if (!empty($content['field_icon_box_section'])): ?>
+            <?php print render($content['field_icon_box_section']); ?>
+        <?php endif; ?>
+    </div>
+
+    <div class="clearfix">
+        <?php if (!empty($content['links'])): ?>
+            <nav class="links node-links clearfix"><?php print render($content['links']); ?></nav>
+        <?php endif; ?>
+
+        <?php print render($content['comments']); ?>
+    </div>
+</article>
